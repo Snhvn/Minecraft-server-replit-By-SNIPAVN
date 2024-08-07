e5a6385fa6bac4acc60e7fa3508153e586aaf658
Snhvn <snhvnnguyen@gmail.com>
2024-08-07 12:58:27 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
-## use java 13 :3
-jabba install openjdk@1.13.0-2
-
-curl "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev" -o server.jar
+## use java 13-19 :3
+jabba install openjdk@1.17.0-2
+## use version 1.16.5 - 1.12.2
+curl "https://cdn.getbukkit.org/spigot/spigot-1.12.2.jar" -o server.jar
 java -Xmx8124 -jar server.jar 
